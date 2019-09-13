a720b676712959f3d8842ba04a5e2458a444de21
extremepayne <harrison.payne@gmail.com>
2019-09-13 17:03:10 +0000

    encapulate in function

--- verse.py
+++ verse.py
@@ -16,6 +16,16 @@ with open(file_path, "r") as scripture_file:
         scriptures.append(verse)
         i += 1
 
-length = len(scriptures)
-choice = random.randint(0, length)
-print(scriptures[choice])
+
+def get_random_verse(volume_id="all"):
+    if volume_id == "all":
+        return random.choice(scriptures)
+    elif isinstance(volume_id, int) and volume_id >=1 and volume_id <= 5:
+        return("unimplemented.")
+    else:
+        raise TypeError
+
+
+rand_verse = get_random_verse()
+print(rand_verse["verse_title"], rand_verse["scripture_text"], sep=': ')
+print(rand_verse)
