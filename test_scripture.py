33a6d8fe3dffe8d5d0d113fa2a9441a356c99f52
extremepayne <harrison.payne@gmail.com>
2020-02-08 14:51:16 -0700

    Made the tests *actually* not fail

--- test_scripture.py
+++ test_scripture.py
@@ -1,10 +1,9 @@
 """Tests scriptures module."""
 import scripture
-import pytest
 
 
 def test_get_random_verse():
     """Tests scripture.get_random_verse() function."""
     my_random_verse = scripture.get_random_verse()
-    assert my_random_verse.text != ""
+    assert my_random_verse != ""
 
