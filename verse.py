40560793ea9fab20fa04590ddc02a07b209b60b2
extremepayne <harrison.payne@gmail.com>
2020-02-09 22:00:07 +0000

    Rename scripture.py

--- verse.py
+++ verse.py
@@ -1,7 +1,7 @@
 """Select a random verse."""
 # pylint: disable=C0103
-import scripture
+import random_verse
 
-rand_verse = scripture.get_random_verse()
+rand_verse = random_verse.get_random_verse()
 print("Random verse: ")
-print(scripture.pretty_print_verse(rand_verse))
+print(random_verse.pretty_print_verse(rand_verse))
